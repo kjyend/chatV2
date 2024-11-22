ab61157829a0064654f1c4047453052865e3be90
junyoung <adad568@naver.com>
2024-11-22 20:35:47 +0900

    feat(roommanager): room 추가 메서드 추가

--- src/server/RoomManager.java
+++ src/server/RoomManager.java
@@ -1,5 +1,12 @@
 package server;
 
+import java.util.ArrayList;
+
 public class RoomManager {
 
+    private final ArrayList<Room> rooms = new ArrayList<>();
+
+    public synchronized void addRoom(Room room) {
+        rooms.add(room);
+    }
 }
