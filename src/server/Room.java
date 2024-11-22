14dabeea957b5f987c11642419543d667fbe3f58
junyoung <adad568@naver.com>
2024-11-22 21:11:40 +0900

    feat(room): 생성자, 필드 생성

--- src/server/Room.java
+++ src/server/Room.java
@@ -1,5 +1,19 @@
 package server;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
 
+    private final String name;
+    private final String password;
+    private final RoomManager roomManager;
+    private List<Session> sessions = new ArrayList<>();
+
+    public Room(String name, String password, RoomManager roomManager) {
+        this.name = name;
+        this.password = password;
+        this.roomManager = roomManager;
+        roomManager.addRoom(this);
+    }
 }
