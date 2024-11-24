5f2cab0e3766112732c7c882b6dff9dff073c2f8
junyoung <adad568@naver.com>
2024-11-24 21:02:45 +0900

    feat(entercommand): 방 입장 메서드 추가

---
+++ src/server/command/EnterCommand.java
@@ -0,0 +1,28 @@
+package server.command;
+
+import server.Room;
+import server.RoomManager;
+import server.Session;
+
+import java.io.IOException;
+
+public class EnterCommand implements Command {
+
+    private final RoomManager roomManager;
+
+    public EnterCommand(RoomManager roomManager) {
+        this.roomManager = roomManager;
+    }
+
+    @Override
+    public void execute(String[] args, Session session) throws IOException {
+        String message = args[1];
+
+        Room room = roomManager.getRooms().stream().filter(r -> r.getName().equals(message))
+                .findFirst().orElseThrow(() -> new IOException("같은 이름의 방이 없습니다: " + message));
+
+        room.addSession(session);
+
+        room.sendRoomAll(session.getUsername() + "님이 입장했습니다.");
+    }
+}
