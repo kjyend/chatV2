48d43ce432c8a0d3d935a74b5e2ac21fd6dd2269
junyoung <adad568@naver.com>
2024-11-24 21:45:37 +0900

    feat(roomscommand): 전체 방 조회 메서드 추가

---
+++ src/server/command/RoomsCommand.java
@@ -0,0 +1,28 @@
+package server.command;
+
+import server.Room;
+import server.RoomManager;
+import server.Session;
+
+import java.io.IOException;
+import java.util.List;
+
+public class RoomsCommand implements Command {
+
+    private final RoomManager roomManager;
+
+    public RoomsCommand(RoomManager roomManager) {
+        this.roomManager = roomManager;
+    }
+
+    @Override
+    public void execute(String[] args, Session session) throws IOException {
+        List<Room> rooms = roomManager.getRooms();
+        StringBuilder sb = new StringBuilder();
+        sb.append("전체 방 갯수 :").append(rooms.size()).append("\n");
+        for (Room room : rooms) {
+            sb.append(" - ").append(room).append("\n");
+        }
+        session.send(sb.toString());
+    }
+}
