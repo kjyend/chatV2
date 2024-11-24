8a59e723f2e057f88cfd6fea421cc7ccc872fd53
junyoung <adad568@naver.com>
2024-11-24 21:39:11 +0900

    feat(enterCommand): 비밀번호 검증 로직 추가

--- src/server/command/EnterCommand.java
+++ src/server/command/EnterCommand.java
@@ -21,6 +21,12 @@ public class EnterCommand implements Command {
         Room room = roomManager.getRooms().stream().filter(r -> r.getName().equals(message))
                 .findFirst().orElseThrow(() -> new IOException("같은 이름의 방이 없습니다: " + message));
 
+        if (!room.getPassword().isEmpty()) {
+            if (room.getPassword().equals(args[2])) {
+                throw new IllegalArgumentException("비밀번호가 맞지 않습니다.");
+            }
+        }
+
         room.addSession(session);
 
         room.sendRoomAll(session.getUsername() + "님이 입장했습니다.");
