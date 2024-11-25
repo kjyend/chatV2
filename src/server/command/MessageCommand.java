aa12b7167009d1df8914eba82697f332d6da5890
junyoung <adad568@naver.com>
2024-11-25 21:21:45 +0900

    refactor(messagecommand): 방 채팅으로 기능 변경

--- src/server/command/MessageCommand.java
+++ src/server/command/MessageCommand.java
@@ -1,21 +1,29 @@
 package server.command;
 
+import server.Room;
+import server.RoomManager;
 import server.Session;
-import server.SessionManager;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MessageCommand implements Command {
 
-    private final SessionManager sessionManager;
+    private final RoomManager roomManager;
 
-    public MessageCommand(SessionManager sessionManager) {
-        this.sessionManager = sessionManager;
+    public MessageCommand(RoomManager roomManager) {
+        this.roomManager = roomManager;
     }
+
 
     @Override
     public void execute(String[] args, Session session) throws IOException {
         String message = args[1];
-        sessionManager.sendAll("[" + session.getUsername() + "] " + message);
+        List<Room> rooms = roomManager.getRooms();
+        for (Room room : rooms) {
+            if (session.getRoomName().equals(room.getName())) {
+                room.sendRoomAll("[" + session.getUsername() + "] " + message);
+            }
+        }
     }
 }
