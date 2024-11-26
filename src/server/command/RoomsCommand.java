af69b23aa2a7e7b5f8a710ceba3abe1254a8c1a5
junyoung <adad568@naver.com>
2024-11-26 21:48:11 +0900

    fix(roomscommand): 방 이름 출력 메서지 변경

--- src/server/command/RoomsCommand.java
+++ src/server/command/RoomsCommand.java
@@ -19,9 +19,9 @@ public class RoomsCommand implements Command {
     public void execute(String[] args, Session session) throws IOException {
         List<Room> rooms = roomManager.getRooms();
         StringBuilder sb = new StringBuilder();
-        sb.append("전체 방 갯수 :").append(rooms.size()).append("\n");
+        sb.append("전체 방 갯수 : ").append(rooms.size()).append("\n");
         for (Room room : rooms) {
-            sb.append(" - ").append(room).append("\n");
+            sb.append(" - ").append(room.getName()).append("\n");
         }
         session.send(sb.toString());
     }
